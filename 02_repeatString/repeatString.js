81d07154a079fa258c8116ba3b9e500fd2402f5e
vagoston <agoston.vilmos@gmail.com>
2022-06-05 15:36:40 +0100

    Add solution repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    result = "";
+    for (let copies = 0; copies < num; copies++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
