191b82db77b502322931646829baeb5c64df0b1a
vagoston <agoston.vilmos@gmail.com>
2022-06-05 17:22:23 +0100

    Add solution tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fTemperaure) {
+  return Math.round(((fTemperaure - 32) * 5 /9) * 10 ) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemperature) {
+  return Math.round(((cTemperature * 9 / 5 ) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
