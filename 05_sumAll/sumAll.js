e0917d956067dc913b22fc32c1e3dc1a491e4135
vagoston <agoston.vilmos@gmail.com>
2022-06-05 16:53:19 +0100

    Add solution sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) {return "ERROR"}
+    lowerLimit = number1 < number2 ? number1 : number2;
+    upperLimit = number1 > number2 ? number1 : number2;
+    if (lowerLimit < 0) return "ERROR";
+    let sum = 0;
+    let currentNumber = lowerLimit;
+    while (currentNumber <= upperLimit){
+        sum += currentNumber;
+        currentNumber++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
