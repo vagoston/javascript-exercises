9a0b1a980ef4fdd7a7cf05fb380f465789fb0253
vagoston <agoston.vilmos@gmail.com>
2022-06-05 17:04:25 +0100

    Add solution leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (((year % 4) || !(year % 100)) && (year % 400))  return false;
+    return true;
 };
 
 // Do not edit below this line
