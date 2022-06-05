f16cfff5fecfec7f5e41125c5ae52f2803c0f116
vagoston <agoston.vilmos@gmail.com>
2022-06-05 16:28:24 +0100

    Add solution removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, values) {
+    listToRemove = [...arguments].slice(1)
+    
+    return array.filter(x=>!listToRemove.includes(x));
 };
 
 // Do not edit below this line
