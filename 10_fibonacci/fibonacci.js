ec87abeb932d1cc06d63a811860cd0c398e59920
vagoston <agoston.vilmos@gmail.com>
2022-06-07 13:18:28 +0100

    Add solution fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n == 0) return 0;
+    if (n < 0) return "OOPS"
+    let a = 0;
+    let b = 1;
+    while(n > 1) {
+        let oldB = b;
+        b += a;
+        a = oldB;
+        --n;
+    }
+    return b;
 };
 
 // Do not edit below this line
