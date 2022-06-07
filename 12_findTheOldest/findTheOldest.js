1829112e0a8a01833a1bf3d04f6b092130306fe5
vagoston <agoston.vilmos@gmail.com>
2022-06-07 13:34:55 +0100

    Add solution getTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
+const age = person => ("yearOfDeath" in person ? person.yearOfDeath : (new Date()).getFullYear()) - person.yearOfBirth;
 
-};
+
+
+const findTheOldest = people => people.reduce((oldest, next) => age(oldest) > age(next) ? oldest : next);
 
 // Do not edit below this line
 module.exports = findTheOldest;
