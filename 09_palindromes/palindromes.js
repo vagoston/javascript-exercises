1e872b180c217ba8ceeaab322145c8dbfaccadfb
vagoston <agoston.vilmos@gmail.com>
2022-06-07 12:58:18 +0100

    Add solution palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let charArray = string.toLowerCase().replace( /[^a-z]/g, '').split('');
+    return charArray.join() === charArray.reverse().join();
 };
 
 // Do not edit below this line
